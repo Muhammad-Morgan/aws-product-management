d8b5034e269c5e67c4a0c25affc47d1246d91c4f
Muhammad Morgan <muhammadmorgan589@gmail.com>
2026-01-27 15:53:05 +0200

    set up config for dynamoDB, and initializing stacks in bin/product-management.ts

--- lib/product-management-stack.ts
+++ lib/product-management-stack.ts
@@ -1,16 +1,18 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as apigatewayv2 from "aws-cdk-lib/aws-apigatewayv2";
+import * as apigatewayv2_integrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambdaRuntime from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as path from "path";
+import { ProductManagementDB } from "./dynamodb-stack";
+type ProductManagementProps = {
+  dynamodbStack: ProductManagementDB;
+} & cdk.StackProps;
 export class ProductManagementStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ProductManagementProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductManagementQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
