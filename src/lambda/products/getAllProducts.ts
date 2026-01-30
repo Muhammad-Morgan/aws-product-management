93ba51129bc80f7d7a00eda66b8e0ba4f49e9efe
Muhammad Morgan <muhammadmorgan589@gmail.com>
2026-01-30 15:07:50 +0200

    setup getAllProducts

--- src/lambda/products/getAllProducts.ts
+++ src/lambda/products/getAllProducts.ts
@@ -1,10 +1,41 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { ProdctRecord } from "../../types/products";
+
+// Initialize AWS clients
+const dynamoClient = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(dynamoClient);
+
+// Environment variables
+const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME!;
+
 export async function handler(
   event: APIGatewayProxyEventV2,
 ): Promise<APIGatewayProxyResultV2> {
-  console.log(event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "get all products" }),
-  };
+  console.log("Event received : ", event);
+  try {
+    // scan the table
+    const scanResult = await docClient.send(
+      new ScanCommand({
+        TableName: PRODUCTS_TABLE_NAME,
+      }),
+    );
+    const products: ProdctRecord[] = (scanResult.Items as ProdctRecord[]) || [];
+    products.sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+    );
+    console.log(`Retrieve products from DynamoDB: ${products.length}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    console.error("Error retrieving products : ", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal server error" }),
+    };
+  }
 }
